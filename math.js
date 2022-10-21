788ead4c05f6d325ec114ab8d1dc3da3ee0c982d
Arnol Plazas <plazasarnol@gmail.com>
2022-10-21 13:25:35 -0500

    crear los prototipos rectangle y cricle con dos metedos que permiten calcular el area y el perimetr junto con sus getters, ademas crear un objeto de cada uno para su validación.

---
+++ math.js
@@ -0,0 +1,57 @@
+class Rectangle {
+    constructor(base, height) {
+      this.base = base;
+      this.height = height;
+    }
+  
+    get perimeter() {
+      return `${this.calcPerimeter(this.base, this.height)}cm`;
+    }
+  
+    get area() {
+      return `${this.calcArea(this.base, this.height)}cm2`;
+    }
+  
+    calcPerimeter(base, height) {
+      return base * 2 + height * 2;
+    }
+  
+    calcArea(base, height) {
+      return base * height;
+    }
+  }
+  
+  class Circle {
+    constructor(radio) {
+      this.radio = radio;
+    }
+  
+    get perimeter() {
+      return `${this.calcPerimeter(this.radio)}cm`;
+    }
+  
+    get area() {
+      return `${this.calcArea(this.radio)}cm2`;
+    }
+  
+    calcPerimeter(radio) {
+      return 2 * Math.PI * radio;
+    }
+  
+    calcArea(radio) {
+      return Math.PI * radio ** 2;
+    }
+  }
+
+console.group('Rectangle')
+let rectangle1 = new Rectangle(10, 15)
+console.log(rectangle1.perimeter)
+console.log(rectangle1.area)
+console.groupEnd('Rectangle')
+
+console.group('Circle')
+let circle1 = new Circle(5)
+console.log(circle1.perimeter)
+console.log(circle1.area)
+console.groupEnd('Circle')
+
