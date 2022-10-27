dcdf75217ee8fa3964decfae1c45c317c187e9c3
Arnol Plazas <plazasarnol@gmail.com>
2022-10-27 09:47:45 -0500

    Funcion de calcular la media de un array desordenado.

--- estadistica.js
+++ estadistica.js
@@ -15,3 +15,39 @@ function calculateAverage2(array) {
     const average = sumArray/array.length
     return average
 }
+
+function isEven(array){
+     return !(array.length%2)
+}
+
+function isOdd(array){
+    return array.length%2
+}
+
+function calculateMedian(array){
+    sortArray = array.sort((accValue, newValue)=>{
+        // if(accValue > newValue){
+        //     return 1
+        // } else if(accValue == newValue){
+        //     return 0
+        // } else if(accValue < newValue){
+        //     return -1
+        // }
+
+        return accValue - newValue
+
+    })
+    console.log(sortArray);
+    const arrayIsEven = isEven(sortArray)
+    let medianArray 
+    if(arrayIsEven){
+        const halfValue1 = sortArray[(sortArray.length/2) -1]
+        const halfValue2 = sortArray[sortArray.length/2]
+        medianArray = calculateAverage([halfValue1, halfValue2])
+    } else{
+        const halfValueIndex = Math.floor(sortArray.length/2)
+        medianArray = sortArray[halfValueIndex]
+    }
+
+    return medianArray
+}
